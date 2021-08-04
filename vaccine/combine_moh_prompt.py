d1b2cce0b05152fc0af54c3fc13c3f665c6373f3
porames <porames.vat@student.mahidol.edu>
2021-08-04 20:38:22 +0700

    remove pop data

--- vaccine/combine_moh_prompt.py
+++ vaccine/combine_moh_prompt.py
@@ -15,8 +15,6 @@ if __name__ == '__main__':
     first_dose = json_load(os.path.join(vaccination_data_dir, "1st-dose-provincial-vaccination.json"))
     second_dose = json_load(os.path.join(vaccination_data_dir, "2nd-dose-provincial-vaccination.json"))
     third_dose = json_load(os.path.join(vaccination_data_dir, "3rd-dose-provincial-vaccination.json"))
-    population_data = json_load("../population-data/th-census-with-hidden-pop.json")
-    age_group_data = json_load("../population-data/th-censusage-group.json")
     if len({first_dose["update_date"], second_dose["update_date"], third_dose["update_date"]}) != 1:
         print("Update date not matched!")
         raise AssertionError
@@ -24,8 +22,6 @@ if __name__ == '__main__':
     df_first_dose = pd.DataFrame(first_dose["data"]).set_index("province").T
     df_second_dose = pd.DataFrame(second_dose["data"]).set_index("province").T
     df_third_dose = pd.DataFrame(third_dose["data"]).set_index("province").T
-    df_population = pd.DataFrame(population_data)
-    df_age_group = pd.DataFrame(age_group_data)
     manufacturer = ["AstraZeneca", "Johnson & Johnson", "Sinopharm", "Sinovac", "total_doses"]
     df_all_dose = df_first_dose.loc[manufacturer, :] + df_second_dose.loc[manufacturer, :] + df_third_dose.loc[manufacturer, :]
     
@@ -38,7 +34,6 @@ if __name__ == '__main__':
                 "2nd_dose": df_second_dose[province].to_dict(),
                 "3rd_dose": df_third_dose[province].to_dict(),
                 "all_dose": df_all_dose[province].to_dict(),
-                "population_data": population_data[population_data["province"] == province][["population", "estimated_living_population"]]
             }
             for province in sorted(df_first_dose.columns)
         ],
