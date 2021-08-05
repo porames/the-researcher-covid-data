7ee31d3ab0558181d0b0879edab0d83802b63d82
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2021-08-05 19:06:08 +0700

    refactor: rearrage functions

--- vaccine/build_timeseries.py
+++ vaccine/build_timeseries.py
@@ -9,9 +9,42 @@ def json_load(file_path: str) -> dict:
         return json.load(json_file)
 
 
+def calculate_national_sum_today(data: dict) -> pd.DataFrame:
+    df = pd.DataFrame(data["data"])
+    today_national_sum = pd.DataFrame(index=[pd.to_datetime(data["update_date"]).floor("D")], data={
+        "first_dose": df["total_1st_dose"].to_numpy().sum(),
+        "second_dose": df["total_2nd_dose"].to_numpy().sum(),
+        "third_dose": df["total_3rd_dose"].to_numpy().sum(),
+        "total_doses": df[["total_1st_dose", "total_2nd_dose", "total_3rd_dose"]].to_numpy().sum(),
+    })  # Numpy sum is faster (even faster than pandas sum)
+    today_national_sum.index.name = "date"
+    return today_national_sum
+
+
+def calculate_manufacturer_sum(data: dict) -> dict:
+    AstraZeneca = 0
+    Sinovac = 0
+    Sinopharm = 0
+    JnJ = 0
+    Pfizer = 0
+    for province in data["data"]:
+        AstraZeneca += province["AstraZeneca"]
+        Sinovac += province["Sinovac"]
+        Sinopharm += province["Sinopharm"]
+        JnJ += province["Johnson & Johnson"]
+        Pfizer += province["Pfizer"]
+    return {
+        "AstraZeneca": AstraZeneca,
+        "Sinovac": Sinovac,
+        "Sinopharm": Sinopharm,
+        "Johnson & Johnson": JnJ,
+        "Pfizer": Pfizer,
+    }
+
+
 def build_manufacturer_timeseries(manufacturer_data: dict) -> pd.DataFrame:
     update_date = manufacturer_data["update_date"]
-    manufacturer_data = calculate_mf_sum(manufacturer_data)
+    manufacturer_data = calculate_manufacturer_sum(manufacturer_data)
     manufacturer_data["date"] = pd.to_datetime(update_date).floor("D")
     START_DATE = "2021-07-02"
     mf_ts = pd.read_json(MAIN_URL + "/vaccination/vaccine-manufacturer-timeseries.json")
@@ -37,39 +70,6 @@ def build_manufacturer_timeseries(manufacturer_data: dict) -> pd.DataFrame:
     mf_ts = old_df.append(new_df, ignore_index=True)
     mf_ts = mf_ts.fillna(0)
     return mf_ts
-
-
-def calculate_national_sum_today(data: dict) -> pd.DataFrame:
-    df = pd.DataFrame(data["data"])
-    today_national_sum = pd.DataFrame(index=[pd.to_datetime(data["update_date"]).floor("D")], data={
-        "first_dose": df["total_1st_dose"].to_numpy().sum(),
-        "second_dose": df["total_2nd_dose"].to_numpy().sum(),
-        "third_dose": df["total_3rd_dose"].to_numpy().sum(),
-        "total_doses": df[["total_1st_dose", "total_2nd_dose", "total_3rd_dose"]].to_numpy().sum(),
-    })  # Numpy sum is faster (even faster than pandas sum)
-    today_national_sum.index.name = "date"
-    return today_national_sum
-
-
-def calculate_mf_sum(data):
-    AstraZeneca = 0
-    Sinovac = 0
-    Sinopharm = 0
-    JnJ = 0
-    Pfizer = 0
-    for province in data["data"]:
-        AstraZeneca += province["AstraZeneca"]
-        Sinovac += province["Sinovac"]
-        Sinopharm += province["Sinopharm"]
-        JnJ += province["Johnson & Johnson"]
-        Pfizer += province["Pfizer"]
-    return {
-        "AstraZeneca": AstraZeneca,
-        "Sinovac": Sinovac,
-        "Sinopharm": Sinopharm,
-        "Johnson & Johnson": JnJ,
-        "Pfizer": Pfizer,
-    }
 
 
 if __name__ == "__main__":
@@ -103,7 +103,10 @@ if __name__ == "__main__":
         force_ascii=False,
     )
     vaccination_timeseries.to_csv("../dataset/national-vaccination-timeseries.csv", index=False)
+    print("Processed National Timeseries")
+    # End of national data processing
 
+    # Begin manufacturer data processing
     mf_data = json_load("../wiki/vaccination/provincial-vaccination-by-manufacturer.json")
 
     manufacturer_timeseries = build_manufacturer_timeseries(mf_data)
@@ -133,7 +136,5 @@ if __name__ == "__main__":
         indent=2,
         force_ascii=False,
     )
-    manufacturer_timeseries.to_csv(
-        "../dataset/vaccine-manufacturer-timeseries.csv"
-    )
-    print("success")
+    manufacturer_timeseries.to_csv("../dataset/vaccine-manufacturer-timeseries.csv")
+    print("Processed Manufacturer Timeseries")
