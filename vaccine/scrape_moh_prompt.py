e924968443a05f1705a1337f8391140f8fdd8c7c
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2021-11-23 00:46:00 +0700

    fix: set webdriver to headless mode

--- vaccine/scrape_moh_prompt.py
+++ vaccine/scrape_moh_prompt.py
@@ -12,7 +12,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 firefox_options = webdriver.FirefoxOptions()
-# firefox_options.add_argument("--headless")
+firefox_options.add_argument("--headless")
 firefox_options.add_argument("--no-sandbox")
 firefox_options.add_argument("--disable-dev-shm-usage")
 
